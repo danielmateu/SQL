7b6b9f6989832e1a7d34fed68cba376a0ec5ad00
Daniel Mateu <danielmateu86@gmail.com>
2023-11-21 10:45:25 +0100

    llaves updated

--- Seccion-05/llaves/llaves.sql
+++ Seccion-05/llaves/llaves.sql
@@ -7,10 +7,10 @@
 -- Hay diferentes tipos de llaves:
 
 -- 1. Llave primaria - primary key
--- 2. Llave foranea - foreign key
--- 3. Llave candidata - candidate key
--- 4. Llave compuesta - composite key
--- 5. Llave Super - super key
+-- 2. Llave candidata - candidate key
+-- 3. Llave Super - super key
+-- 4. Llave foranea - foreign key
+-- 5. Llave compuesta - composite key
 
 -- Hay más, y todas sirven para identificar registros. Entre otras: 
 
@@ -19,3 +19,17 @@
 -- 8. Llave artificial - surrogate key...
 
 -- Llave primaria - primary key
+-- Es un campo o conjunto de campos que identifica de forma única a un registro en una tabla. Una tabla puede tener varios identificadores únicos, pero solo uno puede ser la llave primaria. La llave primaria está basada en los requerimientos.
+
+-- Llave candidata - candidate key
+-- Es un atributo o conjuto de ellos que identifican de forma única. Menos la llave primaria, el resto se consideran claves candidatas.
+
+-- Llave super - super key
+-- Es un conjunto de atributos que puede identificar de forma única. Es un superconjuno de una clave candidata.
+
+-- Llave foranea - foreign key
+-- Las llaves foráneas son usadas para apuntar a la llave primaria de otra tabla. Ayudan a conectar dos tablas con una relación. Dben ser del mismo tipo de datos y longitud.
+
+-- Llave compuesta - composite key
+-- Cuando una clave primaria consta de más de un atributo, se conoce como clave compuesta.
+
