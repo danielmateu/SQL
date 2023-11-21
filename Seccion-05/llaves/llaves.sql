fd605f3ef5f9bb29de60fe3631386c77b8034aad
Daniel Mateu <danielmateu86@gmail.com>
2023-11-21 10:27:47 +0100

    introduccion a las llaves

---
+++ Seccion-05/llaves/llaves.sql
@@ -0,0 +1,21 @@
+-- Introduccion a las llaves
+
+-- Para que las relaciones de las DB sean posibles, necesitamos llaves.
+
+-- Sin las llaves o identidad referencinal, no podemos relacionar las tablas.
+
+-- Hay diferentes tipos de llaves:
+
+-- 1. Llave primaria - primary key
+-- 2. Llave foranea - foreign key
+-- 3. Llave candidata - candidate key
+-- 4. Llave compuesta - composite key
+-- 5. Llave Super - super key
+
+-- Hay más, y todas sirven para identificar registros. Entre otras: 
+
+-- 6. Llave alterna - alternate key
+-- 7. Llave natural - natural key
+-- 8. Llave artificial - surrogate key...
+
+-- Llave primaria - primary key
---
+++ Seccion-05/relaciones/relaciones.sql
@@ -0,0 +1,33 @@
+-- Relaciones
+
+-- Tipos de relaciones entre tablas
+
+-- 1. Relación uno a uno - one to one
+-- 2. Relación uno a muchos - one to many
+-- 3. Relación muchos a muchos - many to many
+-- 4. Relaciones a si misma - self joining relationship
+
+-- Relación uno a uno - one to one
+
+-- Ejemplo: Una persona tiene un pasaporte
+-- Ejemplo: Un usuario tiene un perfil
+-- Ejemplo: Un usuario tiene una dirección de envío
+
+-- Relación uno a muchos - one to many
+
+-- Ejemplo: Un usuario tiene muchas direcciones de envío
+-- Ejemplo: Un usuario tiene muchas órdenes de compra
+-- Ejemplo: Un usuario tiene muchas tarjetas de crédito
+
+-- Relación muchos a muchos - many to many
+
+-- Ejemplo: Un usuario tiene muchos productos en su carrito
+-- Ejemplo: Un usuario tiene muchos productos en su wishlist
+-- Ejemplo: Un usuario tiene muchos productos en su historial de compras
+
+-- Relaciones a si misma - self joining relationship
+
+-- Ejemplo: Un usuario tiene muchos amigos
+-- Ejemplo: Un usuario tiene muchos seguidores
+-- Ejemplo: Un usuario tiene muchos seguidos
+
